172f564cada87f3f2c7b618d491cacc36d713b38
Abdisalam Abdulahi <iidlex99@gmail.com>
2024-06-05 17:30:33 +0300

    adding from_json_string method to the base class

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -39,11 +39,22 @@ class Base:
         """
         if list_objs is None:
             list_objs = []
+
         Jison = []
         for i in list_objs:
             Jison.append(i.to_dictionary())
+
         Jison = cls.to_json_string(Jison)
         clsNmae = cls.__name__ + ".json"
         f = open(clsNmae, "w")
         f.write(Jison)
         f.close()
+
+    @staticmethod
+    def from_json_string(json_string):
+        """
+        returns the list of the JSON string representation
+        """
+        if json_string is None:
+            return []
+        return json.loads(json_string)
