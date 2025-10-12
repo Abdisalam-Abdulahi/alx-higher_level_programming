5e1d06a92145a6483becce2742403d68a482b798
Abdisalam-Abdulahi <iidlex99@gmail.com>
2025-10-12 09:07:11 +0300

    8

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def uppercase(str):
+    """
+    makes str uppercase
+    """
+    for i in str:
+        if (ord(i) >= 97) and (ord(i) <= 122):
+            upper = chr(ord(i) - 32)
+        else:
+            upper = i
+        print("{}".format(upper), end="")
+    print()
