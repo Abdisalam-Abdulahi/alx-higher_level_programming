ec1505a0d73e31f394a75474050e631ced078008
Abdisalam-Abdulahi <iidlex99@gmail.com>
2025-10-22 06:56:23 +0300

    6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -6,5 +6,7 @@ def print_matrix_integer(matrix=[[]]):
     """
     for i in range(len(matrix)):
         for k in range(len(matrix[i])):
-            print("{:d}".format(matrix[i][k]), end=" ")
+            print("{:d}".format(matrix[i][k]), end="")
+            if k != len(matrix[i]) - 1:
+                print(" ", end="")
         print()
