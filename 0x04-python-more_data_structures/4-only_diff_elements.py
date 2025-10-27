1b5e192a32334fc7849f64ae493cd5dd70ee111f
Abdisalam-Abdulahi <iidlex99@gmail.com>
2025-10-27 09:07:49 +0300

    4

---
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def only_diff_elements(set_1, set_2):
+    """
+    returns a set of all elements present in only one set.
+    """
+    diff = []
+    for i in set_1:
+        if i not in set_2:
+            diff.append(i)
+    for i in set_2:
+        if i not in set_1:
+            diff.append(i)
+    return diff
