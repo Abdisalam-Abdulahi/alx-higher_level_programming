ac2ca7bfaf62c4f17b976671b149485a4198eae5
Abdisalam-Abdulahi <iidlex99@gmail.com>
2025-10-27 08:57:39 +0300

    3

---
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def common_elements(set_1, set_2):
+    """
+    returns a set of common elements in two sets.
+    """
+    common = []
+    for i in set_1:
+        if i in set_2:
+            common.append(i)
+
+    return common
