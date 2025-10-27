8d008ff3ed3a99759b8b3aeb4f3031d4456e0a1a
Abdisalam-Abdulahi <iidlex99@gmail.com>
2025-10-27 08:47:34 +0300

    2

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def uniq_add(my_list=[]):
+    """
+    adds all unique integers in a list
+    """
+    my_list = list(set(my_list))
+    sum = 0
+
+    for i in range(len(my_list)):
+        sum += my_list[i]
+
+    return sum
