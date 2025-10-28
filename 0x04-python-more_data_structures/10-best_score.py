5a62a51b44921a410e80a5491e4e0b4624d33f36
Abdisalam-Abdulahi <iidlex99@gmail.com>
2025-10-28 07:49:10 +0300

    10

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+def best_score(a_dictionary):
+    """
+    returns a key with the biggest integer value.
+    """
+    maxi = 0
+    max_key = ""
+    if a_dictionary is None:
+        return None
+
+    for k, v in a_dictionary.items():
+        if v > maxi:
+            maxi = v
+            max_key = k
+    return max_key
