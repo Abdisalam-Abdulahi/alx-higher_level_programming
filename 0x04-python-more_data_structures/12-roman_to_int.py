2c5387f88ebc44431dc4c9d77f85374485bbb327
Abdisalam-Abdulahi <iidlex99@gmail.com>
2025-10-28 09:46:26 +0300

    12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -6,14 +6,18 @@ def roman_to_int(roman_string):
     """
     if type(roman_string) is not str or roman_string is None:
         return 0
-    special = ["IV", "IX", "XL", "XC", "CD", "CM"]
+    special = {"IV": 4, "IX": 9, "XL": 40, "XC": 90, "CD": 400, "CM": 900}
     basic = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
     sum = 0
-    for i in range(len(special)):
-        if special[i] == roman_string:
-            return basic[roman_string[1]] - basic[roman_string[0]]
+    for k, v in special.items():
+        if k == roman_string:
+            return v
 
-    for i in roman_string:
-        if i in basic:
-            sum += basic[i]
+    for i in range(len(roman_string)):
+        if roman_string[i] in basic:
+            if i == (len(roman_string) - 2) and roman_string[len(roman_string) - 2] + roman_string[len(roman_string) - 1] in special:
+                sum += special[roman_string[len(roman_string) - 2] + roman_string[len(roman_string) - 1]]  
+                return sum
+            else:
+                sum += basic[roman_string[i]]
     return sum
